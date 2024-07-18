ec5c69aca2b875c0e6cb8a1bec7a7f3a7dfe155d
akshayk <akshayk.bnp@gmail.com>
2024-07-18 15:37:31 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss'
+import { Routes, Route } from "react-router-dom";
+import { Home } from './components/home/home';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  )
+}
+
+export default App
